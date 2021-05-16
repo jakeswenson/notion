b32e87c70d0875458a79bd99130c21d3f9b29a07
Jake Swenson <jake@jakeswenson.com>
2021-05-15 21:45:38 -0700

    fix: Test include_str in CI

--- src/lib.rs
+++ src/lib.rs
@@ -114,8 +114,10 @@ mod tests {
         let token = {
             if let Some(token) = std::env::var("NOTION_API_TOKEN").ok() {
                 token
+            } else if let Some(token) = std::fs::read_to_string(".api_token").ok() {
+                token
             } else {
-                include_str!(".api_token").to_string()
+                panic!("No API Token found in environment variable 'NOTION_API_TOKEN'!")
             }
         };
         token.trim().to_string()
