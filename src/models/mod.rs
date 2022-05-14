96f9795cc851bd6bd4f184349798a3a15070c43e
Shashank Pachava <spachava753@gmail.com>
2022-05-13 23:27:25 -0400

    Add caption field to code block
    Add caption field to code block. Create enum CodeLanguage for code block. Reorder code block to reflect documentation

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -262,9 +262,95 @@ pub struct ChildPageFields {
 }
 
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum CodeLanguage {
+    Abap,
+    Arduino,
+    Bash,
+    Basic,
+    C,
+    Clojure,
+    Coffeescript,
+    #[serde(rename = "c++")]
+    CPlusPlus,
+    #[serde(rename = "c#")]
+    CSharp,
+    Css,
+    Dart,
+    Diff,
+    Docker,
+    Elixir,
+    Elm,
+    Erlang,
+    Flow,
+    Fortran,
+    #[serde(rename = "f#")]
+    FSharp,
+    Gherkin,
+    Glsl,
+    Go,
+    Graphql,
+    Groovy,
+    Haskell,
+    Html,
+    Java,
+    Javascript,
+    Json,
+    Julia,
+    Kotlin,
+    Latex,
+    Less,
+    Lisp,
+    Livescript,
+    Lua,
+    Makefile,
+    Markdown,
+    Markup,
+    Matlab,
+    Mermaid,
+    Nix,
+    #[serde(rename = "objective-c")]
+    ObjectiveC,
+    Ocaml,
+    Pascal,
+    Perl,
+    Php,
+    #[serde(rename = "plain text")]
+    PlainText,
+    Powershell,
+    Prolog,
+    Protobuf,
+    Python,
+    R,
+    Reason,
+    Ruby,
+    Rust,
+    Sass,
+    Scala,
+    Scheme,
+    Scss,
+    Shell,
+    Sql,
+    Swift,
+    Typescript,
+    #[serde(rename = "vb.net")]
+    VbNet,
+    Verilog,
+    Vhdl,
+    #[serde(rename = "visual basic")]
+    VisualBasic,
+    Webassembly,
+    Xml,
+    Yaml,
+    #[serde(rename = "java/c/c++/c#")]
+    JavaCAndCPlusPlusAndCSharp,
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
 pub struct CodeFields {
-    pub text: Vec<RichText>,
-    pub language: String,
+    pub rich_text: Vec<RichText>,
+    pub caption: Vec<RichText>,
+    pub language: CodeLanguage,
 }
 
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
@@ -329,15 +415,15 @@ pub enum Block {
         common: BlockCommon,
         toggle: TextAndChildren,
     },
-    ChildPage {
-        #[serde(flatten)]
-        common: BlockCommon,
-        child_page: ChildPageFields,
-    },
     Code {
         #[serde(flatten)]
         common: BlockCommon,
         code: CodeFields,
+    },
+    ChildPage {
+        #[serde(flatten)]
+        common: BlockCommon,
+        child_page: ChildPageFields,
     },
     Equation {
         #[serde(flatten)]
